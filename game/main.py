97f34316a90ac0f180adb754547f13b2bf350508
josiah <josiahd.kitchin@gmail.com>
2025-01-19 00:34:27 -0800

    jfkdslj

--- game/main.py
+++ game/main.py
@@ -75,22 +75,19 @@ async def main():
         if key[pygame.K_SPACE]:
             playerOneChar.set_target(random.choice(li))
             playerOneChar.move_character()
-            #await send_player_swing(id, socket)
-            #playerOneChar.set_target(random.choice(li))
 
             location = random.choice(cases)
             target_x, target_y = location
+            screen.fill((0,0,0))
+            #draw_court()
+            draw_text(f"Player 1: {firstPlayerScore}", font, WHITE, 10, margin + 15, screen)
+            draw_text(f"Player 2: {secondPlayerScore}", font, WHITE, SCREEN_HEIGHT + 425, margin + 15, screen)
+            playerOneChar.draw(screen)
+        
 
             await send_player_swing(id, socket)
             await asyncio.sleep(1)
                 
-        #playerOneChar.move_character()
-        screen.fill((0,0,0))
-        #draw_court()
-        draw_text(f"Player 1: {firstPlayerScore}", font, WHITE, 10, margin + 15, screen)
-        draw_text(f"Player 2: {secondPlayerScore}", font, WHITE, SCREEN_HEIGHT + 425, margin + 15, screen)
-        playerOneChar.draw(screen)
-        
         for event in pygame.event.get(): #checks for game events
             if event.type == pygame.QUIT: #if the exit button is being clicked we will exit the while loop
                 running = False
